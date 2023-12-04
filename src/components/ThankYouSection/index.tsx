ee9fb0fc758f5b63952018943fcf5a6337d71dcb
Phu Nguyen <phu.nguyen@nfq.asia>
2023-12-04 22:11:58 +0700

    Update thankyou section

--- src/components/ThankYouSection/index.tsx
+++ src/components/ThankYouSection/index.tsx
@@ -1,22 +1,35 @@
+'use client';
 import { memo } from 'react';
 import OptimalImage from '../OptimalImage';
 import { ephesis } from '@/app/fonts';
+import { motion } from 'framer-motion';
 
 const ThankYouSection = () => {
   return (
-    <div className="relative min-h-screen bg-primary bg-opacity-80 flex flex-row justify-center items-center">
-      <OptimalImage
-        fill
-        alt="Background image"
-        src={`/images/rght/z4786416031014_cb092759fe0189be1f7d5c2dfd22f2e0.jpg`}
-        quality={50}
-        className="object-cover -z-10 object-center sm:object-bottom"
-        sizes="100vw"
-      />
+    <div className="relative min-h-screen bg-primary bg-opacity-80 flex flex-row justify-center items-center overflow-hidden">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: 20 }}
+        variants={{
+          visible: { scale: 1 },
+          hidden: { scale: 1.5 },
+        }}
+        className="absolute top-0 left-0 right-0 bottom-0"
+      >
+        <OptimalImage
+          alt="Background image"
+          src={`/images/rght/z4786416031014_cb092759fe0189be1f7d5c2dfd22f2e0.jpg`}
+          quality={100}
+          className="object-cover -z-10 object-center sm:object-bottom"
+          sizes="100vw"
+          fill
+        />
+      </motion.div>
       <div className="relative py-10">
         <p
           className={
-            'text-4xl sm:text-5xl text-center text-white ' + ephesis.className
+            'text-5xl sm:text-6xl text-center text-white ' + ephesis.className
           }
         >
           Thiên Phú
@@ -24,6 +37,7 @@ const ThankYouSection = () => {
           &
           <br />
           Phương Vy
+          <br />9 years
         </p>
 
         <OptimalImage
@@ -31,7 +45,7 @@ const ThankYouSection = () => {
           alt=""
           width={200}
           height={100}
-          className="absolute w-[90px] h-full -left-[90px] -top-[0px] -rotate-[130deg]"
+          className="absolute w-[120px] h-full -left-[120px] -top-[0px] -rotate-[130deg]"
           sizes="20vw"
         />
         <OptimalImage
@@ -39,7 +53,7 @@ const ThankYouSection = () => {
           alt=""
           width={200}
           height={100}
-          className="absolute w-[90px] h-full -right-[90px] -top-[0px] rotate-[40deg]"
+          className="absolute w-[120px] h-full -right-[120px] -top-[0px] rotate-[40deg]"
           sizes="20vw"
         />
       </div>
