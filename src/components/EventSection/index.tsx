381461bd79870b93db7f5cc9019760273ffbe7e2
Phu Nguyen <phu.nguyen@nfq.asia>
2023-12-14 15:00:58 +0700

    Fix time

--- src/components/EventSection/index.tsx
+++ src/components/EventSection/index.tsx
@@ -129,7 +129,7 @@ const EventSection = () => {
           >
             {renderEventCard({
               name: 'Nhà trai',
-              date: 'Từ 15h chơi tới sáng 22/12/23 (10/11 Âm lịch)',
+              date: '15h 22/12/23 (18h - bạn bè, Can Tho Office)',
               location:
                 'Dốc cầu trầu hôi đường tỉnh 932, Nhơn Nghĩa A, Châu Thành A, Hậu Giang',
               map: 'https://maps.app.goo.gl/uPr5xJmCpgQTHyHu9',
